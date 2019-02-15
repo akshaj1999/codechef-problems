23931380177fda23b37e9e8c1fe232054e9400bc
Akshaj Juneja <43871535+akshaj1999@users.noreply.github.com>
2019-02-15 06:43:18 +0530

    Add files via upload

---
+++ LUCKFOUR.java
@@ -0,0 +1,72 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.*;
+import java.math.*; 
+class Demo {
+
+ 
+    public static void main(String[] args) throws Exception {
+        FastScanner cin = new FastScanner(System.in);
+         int t = cin.nextInt();
+         StringBuffer ans = new StringBuffer();
+         while(t-->0)
+         {
+            String str = cin.nextLine();
+            int length =str.length();
+            int count =0;
+            for(int i=0;i<length;i++)
+            {
+                if(str.charAt(i)=='4')
+                    count++;
+            }
+        ans.append(count+"\n");
+         }
+         System.out.print(ans);
+
+         }
+   
+
+        
+    static class FastScanner {
+        private BufferedReader reader = null;
+        private StringTokenizer tokenizer = null;
+ 
+        public FastScanner(InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+            tokenizer = null;
+        }
+ 
+        public String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+ 
+        public String nextLine() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    return reader.readLine();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+ 
+            return tokenizer.nextToken("\n");
+        }
+ 
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+         
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+    }
+  }
