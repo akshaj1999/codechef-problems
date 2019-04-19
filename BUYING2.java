30fe5135e243a8b7b31c29c051d2e2858a33ba38
Akshaj Juneja <43871535+akshaj1999@users.noreply.github.com>
2019-04-19 21:53:40 +0530

    Add files via upload

---
+++ BUYING2.java
@@ -0,0 +1,93 @@
+import java.io.*;
+import java.util.StringTokenizer;
+import java.util.*;
+import java.math.*; 
+class Demo{
+ 
+    public static void main(String[] args) throws Exception {
+        FastScanner cin = new FastScanner(System.in);
+        int t = cin.nextInt();
+        StringBuffer ans = new StringBuffer();
+        while(t-->0)
+        {
+            int n = cin.nextInt();
+            int x = cin.nextInt();
+            int arr[] = new int[n];
+            int sum = 0;
+            for(int i=0;i<n;i++)
+            {
+                arr[i] =  cin.nextInt();
+                sum+=arr[i];
+            }
+            int quo = sum/x;
+            int rem = sum%x;
+            if(rem==0)
+            {
+                System.out.println(quo);
+                continue;
+            }
+            else
+            {
+                boolean flag = true;
+              Arrays.sort(arr);
+              for(int i=0;i<n;i++)
+              {
+                if(arr[i]<=rem)
+                {
+                    flag = false;
+                    break;
+                }
+              }
+              if(flag)
+                System.out.println(quo);
+            else
+                System.out.println("-1");
+            }
+
+    }
+        
+   }
+
+        
+    static class FastScanner {
+        private BufferedReader reader = null;
+        private StringTokenizer tokenizer = null;
+ 
+        public FastScanner(InputStream in) {
+            reader = new BufferedReader(new InputStreamReader(in));
+            tokenizer = null;
+        }
+ 
+        public String next() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+ 
+        public String nextLine() {
+            if (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    return reader.readLine();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+ 
+            return tokenizer.nextToken("\n");
+        }
+ 
+        public long nextLong() {
+            return Long.parseLong(next());
+        }
+         
+        public int nextInt() {
+            return Integer.parseInt(next());
+        }
+ 
+    }
+  }
